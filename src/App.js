187831f99b72c7e1d6927ce8755800dcaeef96db
Rucha <rucha.kasture2002@gmail.com>
2022-01-02 16:06:34 +0530

    added-styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Users from './components/Users';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-app">
       <Users />
     </div>
   );
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+// import User from './User';
 import './Users.css'
 
 
@@ -8,9 +9,14 @@ function Users() {
 
     const [users, setUsers]=useState([]);
     const [user, setUser]=useState({
-        title:"Ms.",
-        first:"Rucha",
-        last:"Kasture"
+        name:"Mr. Michael Bowman",
+        streetNumber:"82 ",
+        country:"United Kingdom,",
+        postcode:"123",
+        address: "Alexander Road,  Lisburn, Leicestershire, ",
+        timezone: "+4:30 - Kabul",
+        gender: "Male",
+        profile:"https://randomuser.me/api/portraits/men/81.jpg"
     })
     useEffect(()=>{
         axios.get('https://randomuser.me/api/?inc=gender,name,nat,location,picture,email&results=20')
@@ -18,10 +24,7 @@ function Users() {
             //  const newData = data.data;
              const newData = data.data.results;
              setUsers(newData);
-             // console.log(obj);
-            //  console.log(newData);
              console.log(users);
-            //  return newData;
          })
          .catch(err=>console.log(err));
     })
@@ -29,20 +32,27 @@ function Users() {
     const changeUser=(user1)=>{
         console.log(user1);
         setUser({
-         title:user1.name.title,
-         first:user1.name.first,
-         last:user1.name.last
+         name:`${user1.name.title}  ${user1.name.first}  ${user1.name.last}`,
+         streetNumber:`${user1.location.street.number}`,
+         country:`${user1.location.country}, `,
+         postcode:` ${user1.location.postcode}`,
+         address:`${user1.location.street.name}, ${user1.location.city}, ${user1.location.state}, `,
+         timezone:`${user1.location.timezone.offset} - ${user1.location.timezone.description}`,
+         gender:`${user1.gender==="female"?"Female":"Male"}`,
+         profile:`${user1.picture.large}`
         })
     }
     
     return (
     <div>
         <div className="d-flex flex-column align-items-center">
-            <div>
-              <div className="d-flex">
-                  <div>{user.title}</div>
-                  <div className="mx-1">{user.first} </div>
-                  <div>{user.last}</div>
+            <div className="d-flex bg-light p-5 rounded profile-container justify-content-center my-5">
+                <div><img className="rounded-circle profile-img" src={user.profile} alt="img" /></div>
+              <div className="d-flex flex-column align-items-start">
+                  <div className="text-danger bolder text-decoration-underline h1">{user.name}</div>
+                  <div className="d-flex"><div className="street">{user.streetNumber}, </div><div>{user.address}</div><div className="bolder">{user.country}</div><div>{user.postcode}</div></div>
+                  <div>{user.timezone}</div>
+                  <div className="text-secondary">{user.gender}</div>
               </div>
             </div>
         
@@ -50,15 +60,16 @@ function Users() {
            
             {
                 users.map((user)=>{
+                    
             
                    return (
-                   <div onClick={()=>changeUser(user)}className="m-3 p-2 bg-danger fix-width">
-                       <div className="d-flex">
-                       <div key={user.id}>{user.gender}</div>
+                   <div onClick={()=>changeUser(user)}className="m-3 p-3 bg-light fix-width user-container rounded">
+                       <div className="d-flex text-secondary id">
+                       <div key={user.id} className="">{user.gender} . </div>
                        <div className="mx-2">{user.nat}</div>
                        </div>
-                           <div>{user.name.title} {user.name.first} {user.name.last}</div>
-                    <div>{user.email}</div>
+                           <div className="bolder">{user.name.title} {user.name.first} {user.name.last}</div>
+                    <div className="text-danger email">{user.email}</div>
                     </div>
                     )
             
